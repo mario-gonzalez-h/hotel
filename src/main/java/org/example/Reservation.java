613b65962c10e8e91019d147dfbc4ffc775ae986
Alexandra Labaisse <beatsnmelody1@gmail.com>
2024-05-09 11:08:37 -0500

    Finished Reservation Class

---
+++ src/main/java/org/example/Reservation.java
@@ -0,0 +1,78 @@
+package org.example;
+
+
+import java.util.Objects;
+
+//properties:
+//String roomType
+//double price
+//int numberOfNights
+//boolean isWeekend
+public class Reservation {
+
+    private String roomType;
+
+    private double price;
+
+    private int numberOfNights;
+
+    private boolean isWeekend;
+
+    public Reservation(String roomType, double price, int numberOfNights, boolean isWeekend) {
+        this.roomType = roomType;
+        this.price = price;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setWeekend(boolean weekend) {
+        isWeekend = weekend;
+    }
+
+    public double getReservationTotal(){
+
+        if (Objects.equals(this.roomType, "Double")){
+            this.price = 124.00;
+        }else if(Objects.equals(this.roomType, "King")){
+            this.price = 139.00;
+        }
+
+        double totalReservationCost = this.price * this.numberOfNights;
+
+        if (this.isWeekend){
+            totalReservationCost *= .10;
+        }
+
+        return totalReservationCost;
+
+    }
+
+}
